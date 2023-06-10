e0f9def7b0902a3ea4e6cb079df126351461ede7
morgan <731482764@qq.com>
2023-06-10 14:08:22 +0800

    feat(Config): 支持一键换肤

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page{{ $t('hello') }} <SvgIcon name="logo" size="middle"></SvgIcon></h1>
+    <button @click="changeTheme">切换主题</button>
+    <div class="box"></div>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue';
 import { getBrandList } from '@/api/list';
 
+const currentColor = ref<string>('');
+function changeTheme() {
+  currentColor.value = currentColor.value === '' ? 'green' : '';
+  document.documentElement.setAttribute('theme-color', currentColor.value);
+}
 getBrandList();
 </script>
 <style scoped>
@@ -15,5 +23,11 @@ getBrandList();
     display: flex;
     align-items: center;
   }
+}
+
+.box {
+  background-color: var(--td-brand-color-5);
+  width: 100px;
+  height: 100px;
 }
 </style>
