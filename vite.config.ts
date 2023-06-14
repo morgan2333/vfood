4cbad782956ec62eb5e1110fbd1376b325870629
morgan <731482764@qq.com>
2023-06-15 00:11:39 +0800

    feat(Config): 配置开启gzip打包
    需要后端配置nginx，教程：
    https://www.cnblogs.com/mosaicMask/p/17124823.html

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import { viteMockServe } from 'vite-plugin-mock';
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +30,14 @@ export default defineConfig({
       brotliSize: true,
       emitFile: false,
       open: true, // 如果存在本地服务端口，将在打包后自动展示
+    }),
+    viteCompression({
+      // options
+      verbose: true, // 是否在控制台输出压缩结果
+      disable: false, // 是否禁用
+      threshold: 1024 * 10, // 压缩的门槛大小
+      algorithm: 'gzip', // 压缩的算法
+      ext: '.gz', // 压缩后的文件扩展名
     }),
   ],
   resolve: {
